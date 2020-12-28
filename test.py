3be696cf82f16530339dff21b8c04ee50b2b3f30
Lei Sheng <leis@simba.com>
2020-12-28 15:05:30 -0800

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,22 @@
-print 'Hello World'
+import os
+import sys
+import subprocess
+
+def runCommand(cmd):
+    print (cmd)
+    result = subprocess.Popen(cmd, stderr=subprocess.PIPE, stdout=subprocess.PIPE, shell=True)
+    for line in result.stdout:
+        print (line)
+    for line in result.stderr:
+        print (line)
+    result.poll()
+    if result.returncode != 0:
+        exit(-1)
+
+
+def main():
+    runCommand('docker -v')
+
+
+if __name__ == "__main__":
+    main()
