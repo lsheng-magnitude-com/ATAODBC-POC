6331e2e99f9725361e3a93c00abae5dbb1d094e6
Lei Sheng <leis@simba.com>
2021-01-05 14:54:29 -0800

    Update test.py

--- test.py
+++ test.py
@@ -22,20 +22,19 @@ def runCommandinDocker(cmd):
 
 def main():
     runCommand('docker -v')
-    runCommand('docker pull centos:latest')
-    runCommand('docker run --volume /home:/home --detach --name build_env --net=host centos:latest tail -f /dev/null')
+    runCommand('docker pull simbadevops/centos7-gcc5_5:latest')
+    runCommand('docker run --volume /home:/home --detach --name build_env --net=host simbadevops/centos7-gcc5_5:latest tail -f /dev/null')
     runCommand('docker ps')
     runCommandinDocker('cat /etc/os-release')
-    runCommandinDocker('yum install gcc openssl-devel bzip2-devel -y')
-    runCommandinDocker('yum install wget -y')
-    runCommandinDocker('yum install make -y')
-    runCommandinDocker('wget https://www.python.org/ftp/python/2.7.18/Python-2.7.18.tgz')
-    runCommandinDocker('tar xzf Python-2.7.18.tgz')
-    runCommandinDocker('cd ./Python-2.7.18 && ./configure --enable-optimizations')
-    runCommandinDocker('cd ./Python-2.7.18 && make altinstall')
-    runCommandinDocker('curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py')
-    runCommandinDocker('python2.7 get-pip.py')
-
+    #runCommandinDocker('yum install gcc openssl-devel bzip2-devel -y')
+    #runCommandinDocker('yum install wget -y')
+    #runCommandinDocker('yum install make -y')
+    #runCommandinDocker('wget https://www.python.org/ftp/python/2.7.18/Python-2.7.18.tgz')
+    #runCommandinDocker('tar xzf Python-2.7.18.tgz')
+    #runCommandinDocker('cd ./Python-2.7.18 && ./configure --enable-optimizations')
+    #runCommandinDocker('cd ./Python-2.7.18 && make altinstall')
+    #runCommandinDocker('curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py')
+    #runCommandinDocker('python2.7 get-pip.py')
 
 
 if __name__ == "__main__":
