de4fe363559cf785c0dabad593123abf8e292a67
Lei Sheng <leis@simba.com>
2021-01-11 15:18:46 -0800

    Update test.py

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ import os
 import sys
 import subprocess
 
-def runCommand(cmd):
+def run_command(cmd):
     print (cmd)
     result = subprocess.Popen(cmd, stderr=subprocess.PIPE, stdout=subprocess.PIPE, shell=True)
     for line in result.stdout:
@@ -14,29 +14,39 @@ def runCommand(cmd):
         exit(-1)
 
 
-def runCommandinDocker(cmd):
+def run_command_in_docker(cmd):
     cmd = 'docker exec build_env bash -c ' + '"' + cmd + '"'
     print (cmd)
-    runCommand(cmd)
+    run_command(cmd)
 
 
 def main():
-    runCommand('export')
-    runCommand('docker -v')
-    runCommand('docker pull simbadevops/centos7-gcc5_5:latest')
-    runCommand('docker run --volume /home:/home --detach --name build_env --net=host simbadevops/centos7-gcc5_5:latest tail -f /dev/null')
-    runCommand('docker ps')
-    runCommandinDocker('cat /etc/os-release')
-    runCommandinDocker('cd /home/runner/work/ATA/ATA/Booster && python booster.py test.xml')
-    #runCommandinDocker('yum install gcc openssl-devel bzip2-devel -y')
-    #runCommandinDocker('yum install wget -y')
-    #runCommandinDocker('yum install make -y')
-    #runCommandinDocker('wget https://www.python.org/ftp/python/2.7.18/Python-2.7.18.tgz')
-    #runCommandinDocker('tar xzf Python-2.7.18.tgz')
-    #runCommandinDocker('cd ./Python-2.7.18 && ./configure --enable-optimizations')
-    #runCommandinDocker('cd ./Python-2.7.18 && make altinstall')
-    #runCommandinDocker('curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py')
-    #runCommandinDocker('python2.7 get-pip.py')
+    BAMBOO_PLANNAME, BAMBOO_SHORTJOBNAME = init_github_env()
+    run_command('export')
+    run_command('docker -v')
+    run_command('docker pull simbadevops/centos7-gcc5_5:latest')
+    run_command('docker run --volume /home:/home --env BAMBOO_PLANNAME=' + BAMBOO_PLANNAME + ' --env BAMBOO_SHORTJOBNAME=' + BAMBOO_SHORTJOBNAME + ' --detach --name build_env --net=host simbadevops/centos7-gcc5_5:latest tail -f /dev/null')
+    run_command('docker ps')
+    run_command_in_docker('cat /etc/os-release')
+    run_command_in_docker('cd /home/runner/work/ATA/ATA/Booster && python booster.py test.xml')
+
+
+def init_github_env():
+    repo = os.environ.get('GITHUB_REPOSITORY','').replace("'","")
+    workflow = os.environ.get('GITHUB_WORKFLOW','').replace("'","")
+    ref = os.environ.get('GITHUB_REF').replace("'","")
+    category, product, type = (repo.split('/')[-1]).split('-')
+    plan_type, env = workflow.split('-')
+    branch = ref.split('/')[-1]
+    category = category.strip()
+    product = product.strip()
+    type = type.strip()
+    plan_type = plan_type.strip()
+    env = env.strip()
+    branch = branch.strip()
+    BAMBOO_PLANNAME = plan_type + ' ' + type + ' ' + category + ' - ' + env + ' - ' + product + ' ' + branch
+    BAMBOO_SHORTJOBNAME = os.environ.get('GITHUB_JOB', '').replace("'", "")
+    return BAMBOO_PLANNAME, BAMBOO_SHORTJOBNAME
 
 
 if __name__ == "__main__":
