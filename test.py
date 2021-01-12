6f509201c3e5abcc6874a624b8fd63e8a3bf4f0f
lsheng-magnitude-com <76126345+lsheng-magnitude-com@users.noreply.github.com>
2021-01-11 19:46:15 -0800

    Update test.py

--- test.py
+++ test.py
@@ -21,14 +21,15 @@ def run_command_in_docker(cmd):
 
 
 def main():
-    BAMBOO_PLANNAME, BAMBOO_SHORTJOBNAME = init_github_env()
+    plan_name, job_name = init_github_env()
+    workspace = os.environ.get('GITHUB_WORKSPACE','').replace("'","")
     run_command('export')
     run_command('docker -v')
     run_command('docker pull simbadevops/centos7-gcc5_5:latest')
-    run_command('docker run --volume /home:/home --env BAMBOO_PLANNAME=' + "'" + BAMBOO_PLANNAME + "'" +' --env BAMBOO_SHORTJOBNAME=' + BAMBOO_SHORTJOBNAME + ' --detach --name build_env --net=host simbadevops/centos7-gcc5_5:latest tail -f /dev/null')
+    run_command('docker run --volume /home:/home --env BAMBOO_PLANNAME=' + "'" + plan_name + "'" +' --env BAMBOO_SHORTJOBNAME=' + job_name + ' --detach --name build_env --net=host simbadevops/centos7-gcc5_5:latest tail -f /dev/null')
     run_command('docker ps')
     run_command_in_docker('cat /etc/os-release')
-    run_command_in_docker('cd /home/runner/work/ATA/ATA/Booster && python booster.py test.xml')
+    run_command_in_docker('cd ' + workspace + '/Booster && python booster.py test.xml')
 
 
 def init_github_env():
@@ -44,9 +45,9 @@ def init_github_env():
     plan_type = plan_type.strip()
     env = env.strip()
     branch = branch.strip()
-    BAMBOO_PLANNAME = plan_type + ' ' + type + ' ' + category + ' - ' + env + ' - ' + product + ' ' + branch
-    BAMBOO_SHORTJOBNAME = os.environ.get('GITHUB_JOB', '').replace("'", "")
-    return BAMBOO_PLANNAME, BAMBOO_SHORTJOBNAME
+    plan_name = plan_type + ' ' + type + ' ' + category + ' - ' + env + ' - ' + product + ' ' + branch
+    job_name = os.environ.get('GITHUB_JOB', '').replace("'", "")
+    return plan_name, job_name
 
 
 if __name__ == "__main__":
