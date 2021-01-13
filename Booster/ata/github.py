0333ef7887e4d7bcdeea4700ee6e6768230ede43
Lei Sheng <leis@simba.com>
2021-01-13 13:32:35 -0800

    Create modules for bamboo and github env

--- Booster/ata/github.py
+++ Booster/ata/github.py
@@ -5,77 +5,7 @@ import os
 import platform
 
 
-def translateFromBambooVars(h):
-    ''' translate bamboo variables, stripping BAMBOO_ prefix
-    '''
-    nh = {}
-    for i in h:
-        if i[:7].upper() == 'BAMBOO_':
-            nh[i[7:]] = h[i]
-        else:
-            nh[i] = h[i]
-    return h
-
-def getBambooVar(h, name):
-    ''' Get a bamboo variable. Bamboo may change cases
-    '''
-    bn = 'bamboo_' + name
-    if h.has_key(bn):
-        return h[bn]
-    bn = 'BAMBOO_' + name
-    if h.has_key(bn):
-        return h[bn]
-    bn = 'BAMBOO_' + name.upper()
-    if h.has_key(bn):
-        return h[bn]
-    return ''
-
-def getBambooVars(h, names):
-    ''' Return a subset of bamboo vars, filtered by names, stripping bamboo_
-    '''
-    d = {}
-    for i in names:
-        v = getBambooVar(i)
-        if i != None:
-            d[i] = v
-    return d
-
-def decodePlan(key, name):
-    """ decode Bamboo plan key/name
-        return a dict
-    """
-    pi = dict(zip('project plan branch'.split(' '), re.split(r' - ', name)))
-    pprint(pi)
-    pi['planType'] = key[:4]
-    pi['driverType'] = key[4:5]
-    pi['driverCategory'] = key[5:8]
-    pi['osCode'] = key[9:18]
-    pi['projectFunction'], pi['driverTypeName'], pi['driverCategoryName'] = pi['project'].split(' ')
-    pi['driverGroup'] = pi['driverCategoryName']
-    if pi['planType'] == 'BULD':
-        pi['compilerCode'] = key[18:27]
-        pi['driverBit'] = key[27:29]
-        pi['osName'], pi['osVersion'], pi['compilerName'], pi['compilerVersion'], _ = pi['plan'].split(' ')
-    elif pi['planType'][:3] == 'TST':
-        pi['osBit'] = key[18:20]
-        pi['driverBit'] = key[20:22]
-        pi['dmcode'] = key[22:23]
-        pi['osName'], pi['osVersion'], pi['osArch'], _, pi['dmName'] = pi['plan'].split(' ')
-    elif pi['planType'][:3] == 'PKG':
-        pi['compilerCode'] = key[18:27]
-        pi['driverBit'] = key[27:29]
-        pi['osName'], pi['osVersion'], pi['compilerName'], pi['compilerVersion'], _, pi['pkg'] = pi['plan'].split(' ')
-    pi['driverName'], pi['driverBranch'] = pi['branch'].split(' ')
-    return pi
-
-
 def initPlanSettings(settings):
-    longPlanNameArray = getLongPlanNameArray()
-    projectNameArray = getProjectNameArray(longPlanNameArray)
-    planNameArray = getPlanNameArray(longPlanNameArray)
-    jobNameArray = getJobNameArray()
-    branchNameArray = getBranchNameArray(longPlanNameArray)
-    settings['bamboo'] = getBambooServer()
 
     if getPlatform(planNameArray) is not None:
         settings['platform'] = getPlatform(planNameArray)
@@ -819,26 +749,3 @@ def getBambooCapability(var, bamboo_var):
         message += '\n overriding %s with %s\n'
         logger.critical(message % (var, capability))
         return capability
-
-
-def initCompilerSettings(planSettings):
-    settings = {}
-    #load jdk
-    for envkey in os.environ.keys():
-        if envkey.startswith ('BAMBOO_CAPABILITY_SYSTEM_JDK') and envkey.endswith('X86'):
-            key = envkey.replace('BAMBOO_CAPABILITY_SYSTEM_JDK_','').replace('X86','')
-            settings[key+ '_HOME32'] = os.environ[envkey]
-        if envkey.startswith ('BAMBOO_CAPABILITY_SYSTEM_JDK') and envkey.endswith('X64'):
-            key = envkey.replace('BAMBOO_CAPABILITY_SYSTEM_JDK_','').replace('X64','')
-            settings[key + '_HOME64'] = settings[key + '_HOME'] = os.environ[envkey]
-        if planSettings['platform'] == 'OSX' and envkey.startswith ('BAMBOO_CAPABILITY_SYSTEM_JDK') and not envkey.startswith ('BAMBOO_CAPABILITY_SYSTEM_JDK_JDK_') and not envkey.endswith('JDK'):
-            key = envkey.replace('BAMBOO_CAPABILITY_SYSTEM_JDK_', '')
-            settings[key + '_HOME32'] = settings[key + '_HOME64'] = settings[key + '_HOME'] = os.environ[envkey]
-
-    #load ant
-    if 'BAMBOO_CAPABILITY_SYSTEM_BUILDER_ANT_ANT' in os.environ.keys():
-        settings['ANT_HOME'] = os.environ['BAMBOO_CAPABILITY_SYSTEM_BUILDER_ANT_ANT']
-
-    for key in settings.keys():
-        os.environ['BOOSTER_VAR_' + key] = settings[key]
-    return settings
--- test.py
+++ test.py
@@ -21,33 +21,35 @@ def run_command_in_docker(cmd):
 
 
 def main():
-    plan_name, job_name = init_github_env()
+    docker_env_string = init_github_env()
     workspace = os.environ.get('GITHUB_WORKSPACE','').replace("'","")
     run_command('export')
     run_command('docker -v')
     run_command('docker pull simbadevops/centos7-gcc5_5:latest')
-    run_command('docker run --volume /home:/home --env BAMBOO_PLANNAME=' + "'" + plan_name + "'" +' --env BAMBOO_SHORTJOBNAME=' + job_name + ' --env IS_GITHUB_WORKFLOW=true --detach --name build_env --net=host simbadevops/centos7-gcc5_5:latest tail -f /dev/null')
+    run_command('docker run --volume /home:/home ' + docker_env_string + ' --detach --name build_env --net=host simbadevops/centos7-gcc5_5:latest tail -f /dev/null')
     run_command('docker ps')
     run_command_in_docker('cat /etc/os-release')
     run_command_in_docker('cd ' + workspace + '/Booster && python booster.py test.xml')
 
 
 def init_github_env():
-    repo = os.environ.get('GITHUB_REPOSITORY','').replace("'","")
-    workflow = os.environ.get('GITHUB_WORKFLOW','').replace("'","")
-    ref = os.environ.get('GITHUB_REF').replace("'","")
-    category, product, type = (repo.split('/')[-1]).split('-')
-    plan_type, env = workflow.split('-')
-    branch = ref.split('/')[-1]
-    category = category.strip()
-    product = product.strip()
-    type = type.strip()
-    plan_type = plan_type.strip()
-    env = env.strip()
-    branch = branch.strip()
-    plan_name = plan_type + ' ' + type + ' ' + category + ' - ' + env + ' - ' + product + ' ' + branch
-    job_name = os.environ.get('GITHUB_JOB', '').replace("'", "")
-    return plan_name, job_name
+    repo = os.environ.get('GITHUB_REPOSITORY','')
+    workflow = os.environ.get('GITHUB_WORKFLOW','')
+    ref = os.environ.get('GITHUB_REF')
+    job = os.environ.get('GITHUB_JOB', '')
+    #category, product, type = (repo.split('/')[-1]).split('-')
+    #plan_type, env = workflow.split('-')
+    #branch = ref.split('/')[-1]
+    #category = category.strip()
+    #product = product.strip()
+    #type = type.strip()
+    #plan_type = plan_type.strip()
+    #env = env.strip()
+    #branch = branch.strip()
+    #plan_name = plan_type + ' ' + type + ' ' + category + ' - ' + env + ' - ' + product + ' ' + branch
+    #job_name = os.environ.get('GITHUB_JOB', '').replace("'", "")
+    docker_env_string = '--env GITHUB_REPOSITORY=' + repo + ' --env GITHUB_WORKFLOW=' + workflow + ' --env GITHUB_REF=' + ref + ' --env GITHUB_JOB=' + job
+    return docker_env_string
 
 
 if __name__ == "__main__":
