cb534d2d9dfa99213733848bf5a6f41aa4f22b1a
Lei Sheng <leis@simba.com>
2021-01-13 17:07:21 -0800

    Update github.py

--- Booster/ata/github.py
+++ Booster/ata/github.py
@@ -6,10 +6,44 @@ import platform
 
 
 def initPlanSettings(settings):
-    print os.environ.get('GITHUB_REPOSITORY')
-    print os.environ.get('GITHUB_WORKFLOW')
-    print os.environ.get('GITHUB_REF')
-    print os.environ.get('GITHUB_JOB')
+    repo = os.environ.get('GITHUB_REPOSITORY')
+    workflow = os.environ.get('GITHUB_WORKFLOW')
+    ref = os.environ.get('GITHUB_REF')
+    job = os.environ.get('GITHUB_JOB')
+    category, product, type = (repo.split('/')[-1]).split('-')
+    plan_type, env = workflow.split('-')
+    branch = ref.split('/')[-1]
+    category = category.strip()
+    product = product.strip()
+    type = type.strip()
+    plan_type = plan_type.strip()
+    distribution, compiler, bitness = env.split().strip()
+    branch = branch.strip()
+
+    settings['platform'] = ''
+    settings['project'] = category + type
+    settings['product'] = product
+    settings['product_lower'] = product.lower()
+    settings['branch'] = branch
+    settings['plantype'] = ''
+    settings['distribution'] = distribution
+    settings['compiler'] = compiler
+    settings['plan'] = ''
+    settings['job'] = job
+    settings['buildtarget'] = ''
+    settings['bitness'] = bitness
+    settings['drivertype'] = ''
+    settings['dm_name'] = ''
+    settings['dm_version'] = ''
+    settings['dm'] = ''
+    settings['jre'] = ''
+    settings['test_platform'] = ''
+    settings['package_platform'] = ''
+    settings['os_arch'] = ''
+    settings['testtype'] = ''
+    settings['packagetype'] = ''
+    settings['packageformat'] = ''
+
     return settings
 
 
