24294ac491707f826167ecdc19d7de1e082d6ae2
Lei Sheng <leis@simba.com>
2021-02-01 15:20:15 -0800

    Update SetSecrets.py

--- scripts/SetSecrets.py
+++ scripts/SetSecrets.py
@@ -1,6 +1,26 @@
 import os
+import Remove
+import re
+import sys
+import fileinput
 
-if os.environ.get('USERNAME','unknown') == 'leis':
-    print ('secret set')
-else:
-    print ('secret not set')
+
+def read_env_variable(var):
+    if os.environ.get(var, 'undef') != 'undef':
+        print (var + ' is set')
+    else:
+        print (var + ' not set')
+
+
+def main():
+    SNOWFLAKE_TEST_USER
+    SNOWFLAKE_TEST_PASSWORD
+    SNOWFLAKE_TEST_ACCOUNT
+    SNOWFLAKE_TEST_WAREHOUSE
+    SNOWFLAKE_TEST_DATABASE
+    SNOWFLAKE_TEST_SCHEMA
+    SNOWFLAKE_TEST_ROLE
+
+
+if __name__ == "__main__":
+    main()
