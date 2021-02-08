6d885496c0b8765b409858af2a7f2696352acd6e
lsheng-magnitude-com <76126345+lsheng-magnitude-com@users.noreply.github.com>
2021-02-07 17:51:59 -0800

    Update build_driver.py

--- scripts/build_driver.py
+++ scripts/build_driver.py
@@ -53,8 +53,8 @@ def build_windows():
     run_command("scripts\\setup_php_sdk.bat " + arch + " " + target + " " + vs + " D:\\php-sdk")
     run_command("scripts\\run_setup_php.bat " + arch + " " + target + " " + vs + " " + php + " D:\\php-sdk")
 
-    #print ("====> build pdo driver")
-    #run_command("scripts\\run_build_pdo_snowflake.bat " + arch + " " + target + " " + vs + " " + php + " c:\\php-sdk")
+    print ("====> build pdo driver")
+    run_command("scripts\\run_build_pdo_snowflake.bat " + arch + " " + target + " " + vs + " " + php + " D:\\php-sdk")
     #dll = os.path.join("c:/php-sdk/phpmaster", vs.replace("VS", "vc"), arch, "php-src", arch, target + "_TS", "php_pdo_snowflake.dll")
     #php_ext_dir = os.path.join("c:", "tools", "php", "ext")
     #run_command("xcopy " + dll + php_ext_dir +" /I/Y/F")
